82e203ee0927a1e35dc707253d0b3bfc3c7c9a01
dinhloino1faifai <dinhloino1faifai@gmail.com>
2026-03-20 10:35:39 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,203 @@
+// =========================
+// APP DATA CONFIGURATION
+// =========================
+
+// APP LIST
+const apps = [
+  
+  {
+    name: 'AovCheat ✅',
+    url: 'http://sub.khoindvn.io.vn/aovcheat/',
+    img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/c4/ca/f6/c4caf66c-cd27-efda-fdd5-7f7f87535663/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
+    descriptions: 'Hack Map Đánh Kín Update 06/03/2026'
+  },{
+    name: 'AovMod ✅',
+    url: 'http://sub.khoindvn.io.vn/aovmod/',
+    img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/c4/ca/f6/c4caf66c-cd27-efda-fdd5-7f7f87535663/AppIcon-1x_U007emarketing-0-11-0-85-220-0.png/512x512bb.jpg',
+    descriptions: 'Aov Mod Skin Update 06/03/2026'
+  },{
+    name: 'Delta VN ✅',
+    url: 'https://sub.khoindvn.io.vn/deltavn/',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM4K15INcKnSAx3OEPed4Z8rc9bLg1FH2HEoYr6w8FFg&s',
+    descriptions: 'Update Fix Văng 06/03/2026'
+  },{
+    name: 'SkiBx VN ✅',
+    url: 'https://sub.khoindvn.io.vn/Skibx/',
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQM4K15INcKnSAx3OEPed4Z8rc9bLg1FH2HEoYr6w8FFg&s',
+    descriptions: 'Update Fix Văng 06/03/2026'
+  },{
+    name: '8 Ball Pool ✅',
+    url: 'https://sub.khoindvn.io.vn/skibxvn/',
+    img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/74/5c/61/745c6160-c9d9-804c-d2ae-6a9ecd8f1fe8/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/512x512bb.jpg',
+    descriptions: 'Hack Đuờng Kẻ Update Fix Văng 06/03/2026'
+  },{
+    name: 'Crossfire: Legends ✅',
+    url: 'http://sub.khoindvn.io.vn/cfvn/',
+    img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/d5/67/82/d5678282-ac1b-05d7-c61f-c10955a61479/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
+    descriptions: 'Hack Aim antiband – Update fix văng 06/03/2026'
+  },{
+    name: 'Get Key Crossfire: Legends ✅',
+    url: 'https://authtool.app/get-key?code=7842d3c4-92fa-48a5-8130-8d150ae3a32e',
+    img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple211/v4/d5/67/82/d5678282-ac1b-05d7-c61f-c10955a61479/AppIcon-1x_U007emarketing-0-10-0-85-220-0.png/512x512bb.jpg',
+    descriptions: 'Lấy key Crossfire: Legends'
+  },{
+    name: 'Play Together VNG ✅',
+    url: 'http://sub.khoindvn.io.vn/play/',
+    img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/b1/75/6f/b1756f48-31e2-dbf5-2979-1c1ae0112b3d/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/512x512bb.jpg',
+    descriptions: 'Auto câu cá – Update 06/03/2026'
+  },
+  {
+    name: 'Get Key Play Together VNG ✅',
+    url: 'https://authtool.app/get-key?code=9892c4a7-1fbd-4ced-a262-3e61819c24ad',
+    img: 'https://is1-ssl.mzstatic.com/image/thumb/Purple221/v4/b1/75/6f/b1756f48-31e2-dbf5-2979-1c1ae0112b3d/AppIcon-0-0-1x_U007emarketing-0-8-0-85-220.png/512x512bb.jpg',
+    descriptions: 'Lấy key Play Together VNG'
+  },{
+    name: 'TrollStore X ✅',
+    url: 'https://sub.khoindvn.io.vn/trollst',
+    img: 'https://raw.githubusercontent.com/alfiecg24/TrollInstallerX/main/Resources/Icon.png',
+    descriptions: 'Hỗ trợ iOS 14.0 – 16.6.1'
+  }
+];
+
+// KSIGN LIST
+const ksignApps = [
+  { name: 'KSign Update ✅', url: 'https://api.khoindvn.io.vn/UM2Mcz', img: 'img/pf/ksign.png', descriptions: 'National Oilwell Varco, Inc.' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/enw3mG', img: 'img/pf/ksign.png', descriptions: 'VIETNAM AIRLINES JSC' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/03zH0o', img: 'img/pf/ksign.png', descriptions: 'PowerChina International Group Limited' },
+  { name: 'KSign PowerChina', url: 'https://api.khoindvn.io.vn/xuuKqO', img: 'img/pf/ksign.png', descriptions: 'PowerChina International Group Limited' },
+  { name: 'KSign PowerChina 1', url: 'https://api.khoindvn.io.vn/QdVVh3', img: 'img/pf/ksign.png', descriptions: 'PowerChina International Group Limited' },
+  { name: 'KSign PowerChina 2', url: 'https://api.khoindvn.io.vn/t1UK1D', img: 'img/pf/ksign.png', descriptions: 'PowerChina International Group Limited' },
+  { name: 'KSign PowerChina 3', url: 'https://api.khoindvn.io.vn/NU8PP6', img: 'img/pf/ksign.png', descriptions: 'PowerChina International Group Limited' },
+  { name: 'KSign', url: 'https://api.khoindvn.eu.org/XSCvQT', img: 'img/pf/ksign.png', descriptions: 'Qingdao Rural Commercial Bank Co., Ltd ' }, 
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/g65UJy', img: 'img/pf/ksign.png', descriptions: 'Commission on Elections ' },
+  { name: 'KSign Com V1', url: 'https://api.khoindvn.io.vn/ZsgCVX', img: 'img/pf/ksign.png', descriptions: 'Commission on Elections ' },
+  { name: 'KSign Com V2', url: 'https://api.khoindvn.io.vn/VNNlkL', img: 'img/pf/ksign.png', descriptions: 'Commission on Elections ' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/9VDZ4Y', img: 'img/pf/ksign.png', descriptions: 'Wasu Media & Network Co' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/h5YrG1', img: 'img/pf/ksign.png', descriptions: 'Luoyang Postal Administration' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/TsTFIu', img: 'img/pf/ksign.png', descriptions: 'Etisalat - Emirates Telecommunications Corporation' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/IUurbX', img: 'img/pf/ksign.png', descriptions: 'China National Heavy Duty Truck Group Co., Ltd' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/5PBonm', img: 'img/pf/ksign.png', descriptions: 'China Telecommunications Corporation' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/1H9iee', img: 'img/pf/ksign.png', descriptions: 'Tianjin University of Commerce' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/qYiNnB', img: 'img/pf/ksign.png', descriptions: 'VIETTEL GROUP' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/xwgru5', img: 'img/pf/ksign.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'KSign ChiBa V1', url: 'https://api.khoindvn.io.vn/KYD1nK', img: 'img/pf/ksign.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'KSign ChiBa V2', url: 'https://api.khoindvn.io.vn/EIpxft', img: 'img/pf/ksign.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'KSign ChiBa V3', url: 'https://api.khoindvn.io.vn/ZNciX8', img: 'img/pf/ksign.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'KSign ChiBa V4', url: 'https://api.khoindvn.io.vn/nBQF8h', img: 'img/pf/ksign.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/5dujHz', img: 'img/pf/ksign.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'KSign VN V1', url: 'https://api.khoindvn.io.vn/hZ8MlI', img: 'img/pf/ksign.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'KSign VN V2', url: 'https://api.khoindvn.io.vn/Fdty3X', img: 'img/pf/ksign.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'KSign VN V3', url: 'https://api.khoindvn.io.vn/AK7usE', img: 'img/pf/ksign.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'KSign VN V4', url: 'https://api.khoindvn.io.vn/oIV0oN', img: 'img/pf/ksign.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'KSign', url: 'https://api.khoindvn.io.vn/6DNLtD', img: 'img/pf/ksign.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'KSign GLOBAL V1', url: 'https://api.khoindvn.io.vn/j7XAya', img: 'img/pf/ksign.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'KSign GLOBAL V2', url: 'https://api.khoindvn.io.vn/qNkRtG', img: 'img/pf/ksign.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'KSign GLOBAL V3', url: 'https://api.khoindvn.io.vn/mTt9ip', img: 'img/pf/ksign.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'KSign GLOBAL V4', url: 'https://api.khoindvn.io.vn/RX7IAf', img: 'img/pf/ksign.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'KSign GLOBAL V5', url: 'https://api.khoindvn.io.vn/tGeKc0', img: 'img/pf/ksign.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'KSign GLOBAL V6', url: 'https://api.khoindvn.io.vn/Deq7RD', img: 'img/pf/ksign.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'KSign GLOBAL V7', url: 'https://api.khoindvn.io.vn/kcvpWe', img: 'img/pf/ksign.png', descriptions: 'GLOBAL TAKEOFF, INC' }
+];
+
+// ESIGN LIST
+const esignApps = [
+  { name: 'ESign Update ✅', url: 'https://api.khoindvn.io.vn/RoAzoJ', img: 'img/pf/profile-pic.png', descriptions: 'National Oilwell Varco, Inc.' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/mJoVMn', img: 'img/pf/profile-pic.png', descriptions: 'VIETNAM AIRLINES JSC' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/IDcW4P', img: 'img/pf/profile-pic.png', descriptions: 'PowerChina International Group Limited' },
+  { name: 'ESign PowerChina', url: 'https://api.khoindvn.io.vn/NvH0ZF', img: 'img/pf/profile-pic.png', descriptions: 'PowerChina International Group Limited' },
+  { name: 'ESign PowerChina 1', url: 'https://api.khoindvn.io.vn/GKJa03', img: 'img/pf/profile-pic.png', descriptions: 'PowerChina International Group Limited' },
+  { name: 'ESign PowerChina 2', url: 'https://api.khoindvn.io.vn/lYSU81', img: 'img/pf/profile-pic.png', descriptions: 'PowerChina International Group Limited' },
+  { name: 'ESign PowerChina 3', url: 'https://api.khoindvn.io.vn/bzAdnw', img: 'img/pf/profile-pic.png', descriptions: 'PowerChina International Group Limited' },
+  { name: 'ESign', url: 'https://api.khoindvn.eu.org/KXcveB', img: 'img/pf/profile-pic.png', descriptions: 'Qingdao Rural Commercial Bank Co., Ltd ' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/jAbzrt', img: 'img/pf/profile-pic.png', descriptions: 'Commission on Elections' },
+  { name: 'ESign Com V1', url: 'https://api.khoindvn.io.vn/RzkRFk', img: 'img/pf/profile-pic.png', descriptions: 'Commission on Elections' },
+  { name: 'ESign Com V2', url: 'https://api.khoindvn.io.vn/oC0s5H', img: 'img/pf/profile-pic.png', descriptions: 'Commission on Elections' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/ST2vo0', img: 'img/pf/profile-pic.png', descriptions: 'Luoyang Postal Administration' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/CbhJOR', img: 'img/pf/profile-pic.png', descriptions: 'Wasu Media & Network Co., Ltd' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/Zxycmb', img: 'img/pf/profile-pic.png', descriptions: 'Etisalat - Emirates Telecommunications Corporation' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/11jtJ7', img: 'img/pf/profile-pic.png', descriptions: 'China National Heavy Duty Truck Group Co., Ltd.' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/JJW6Nj', img: 'img/pf/profile-pic.png', descriptions: 'China Telecommunications Corporation' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/6xAzUM', img: 'img/pf/profile-pic.png', descriptions: 'Tianjin University of Commerce' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/Sco2A2', img: 'img/pf/profile-pic.png', descriptions: 'VIETTEL GROUP' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/Q9sxJb', img: 'img/pf/profile-pic.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'ESign ChiBa V1', url: 'https://api.khoindvn.io.vn/Bhqw0c', img: 'img/pf/profile-pic.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'ESign ChiBa V2', url: 'https://api.khoindvn.io.vn/9UqTrd', img: 'img/pf/profile-pic.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'ESign ChiBa V3', url: 'https://api.khoindvn.io.vn/BcUSW0', img: 'img/pf/profile-pic.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'ESign ChiBa V4', url: 'https://api.khoindvn.io.vn/4jftNL', img: 'img/pf/profile-pic.png', descriptions: 'CHIBA INSTITUTE OF TECHNOLOGY' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/z4nsG3', img: 'img/pf/profile-pic.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'ESign VN V1', url: 'https://api.khoindvn.io.vn/hMU3PR', img: 'img/pf/profile-pic.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'ESign VN V2', url: 'https://api.khoindvn.io.vn/OBr8Zi', img: 'img/pf/profile-pic.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'ESign VN V3', url: 'https://api.khoindvn.io.vn/gegf9d', img: 'img/pf/profile-pic.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'ESign VN V4', url: 'https://api.khoindvn.io.vn/a1Gr0d', img: 'img/pf/profile-pic.png', descriptions: 'VIETNAM JOINT STOCK COMMERCIAL BANK FOR INDUSTRY AND TRADE' },
+  { name: 'ESign', url: 'https://api.khoindvn.io.vn/2nf4UO', img: 'img/pf/profile-pic.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'ESign GlOBAL V1', url: 'https://api.khoindvn.io.vn/j6HQG8', img: 'img/pf/profile-pic.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'ESign GlOBAL V2', url: 'https://api.khoindvn.io.vn/hvcTb2', img: 'img/pf/profile-pic.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'ESign GlOBAL V3', url: 'https://api.khoindvn.io.vn/bJV2fn', img: 'img/pf/profile-pic.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'ESign GlOBAL V4', url: 'https://api.khoindvn.io.vn/QQYC0K', img: 'img/pf/profile-pic.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'ESign GlOBAL V5', url: 'https://api.khoindvn.io.vn/ylpuYQ', img: 'img/pf/profile-pic.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'ESign GlOBAL V6', url: 'https://api.khoindvn.io.vn/vnAa35', img: 'img/pf/profile-pic.png', descriptions: 'GLOBAL TAKEOFF, INC' },
+  { name: 'ESign GlOBAL V7', url: 'https://api.khoindvn.io.vn/qYW0nK', img: 'img/pf/profile-pic.png', descriptions: 'GLOBAL TAKEOFF, INC' }
+];
+
+// =========================
+// UI + RENDER
+// =========================
+
+const cloudIcon = `
+<svg style="width:24px;height:24px;fill:currentColor" viewBox="0 0 1024 1024">
+  <path d="M691.3 693.9c-24.7 0-44.8-20-44.8-44.8s20-44.8 44.8-44.8h40"/>
+</svg>
+`;
+
+function renderItem(item) {
+  return `
+  <a href="${item.url}" class="app-row" target="_blank">
+    <img src="${item.img}" class="app-icon" alt="${item.name}">
+    <div class="app-text">
+      <div class="app-title">${item.name}</div>
+      <div class="app-sub">${item.descriptions}</div>
+    </div>
+    <div class="app-action">INSTALL</div>
+  </a>
+  `;
+}
+
+function renderList(selector, list) {
+  const container = document.querySelector(selector);
+  if (!container) return;
+  list.forEach(item => {
+    container.innerHTML += renderItem(item);
+  });
+}
+
+// INIT
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Signed App (PHẦN BẠN BỊ MẤT)
+  renderList('.app-section', apps);
+
+  // KSign / ESign
+  renderList('.ksign-section', ksignApps);
+  renderList('.esign-section', esignApps);
+
+  const btnKsign = document.getElementById('btn-ksign');
+  const btnEsign = document.getElementById('btn-esign');
+
+  const ksignSection = document.querySelector('.ksign-section');
+  const esignSection = document.querySelector('.esign-section');
+
+  btnKsign.onclick = () => {
+    btnKsign.classList.add('active');
+    btnEsign.classList.remove('active');
+    ksignSection.classList.remove('hidden');
+    esignSection.classList.add('hidden');
+  };
+
+  btnEsign.onclick = () => {
+    btnEsign.classList.add('active');
+    btnKsign.classList.remove('active');
+    esignSection.classList.remove('hidden');
+    ksignSection.classList.add('hidden');
+  };
+
+});
